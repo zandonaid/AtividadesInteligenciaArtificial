996f20892080f799acf182cd26ff3381e0fee4f5
Eduardo Fillipe da Silva Reis <eduardo.reis@dcomp.ufs.br>
2019-11-14 20:22:53 -0300

    Commit da classe inicializadora da atividade.

---
+++ AtividadesInteligenciaArtificial/src/main/java/br/ufs/dcomp/eduard6/disciplinas/ia/atividades/atividade01/launcher/Launcher.java
@@ -0,0 +1,12 @@
+package br.ufs.dcomp.eduard6.disciplinas.ia.atividades.atividade01.launcher;
+
+public class Launcher {
+	/**
+	 * @author Eduardo Fillipe da Silva Reis
+	 * 
+	 * Ponto de início da atividade.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Teste");
+	}
+}
