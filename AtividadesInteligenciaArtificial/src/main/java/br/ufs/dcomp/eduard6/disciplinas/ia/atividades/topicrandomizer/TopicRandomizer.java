87f87f4a959e78ee028bb1fba538325c09ac6740
Eduardo Fillipe da Silva Reis <eduardo.reis@dcomp.ufs.br>
2019-11-14 22:30:11 -0300

    -Adicionado TopicRandomizer ao repositório. -Alteração para atividade02 no TopicRandomizer.

---
+++ AtividadesInteligenciaArtificial/src/main/java/br/ufs/dcomp/eduard6/disciplinas/ia/atividades/topicrandomizer/TopicRandomizer.java
@@ -0,0 +1,41 @@
+package br.ufs.dcomp.eduard6.disciplinas.ia.atividades.topicrandomizer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class TopicRandomizer {
+	/**
+	 * @author Eduardo Fillipe da Silva Reis
+	 * Classe que divide uma dada atividade entre os integrantes
+	 * do grupo.
+	 */
+
+	enum Alunos{
+        EDUARDO(), FELIPE(), MARIANA(), MATHEUS();
+        public String topico;
+
+        private Alunos(String topico) {
+            this.topico = topico;
+        }
+        
+        private Alunos() {}
+        
+        @Override
+        public String toString() {
+            return this.name() + ": " + this.topico;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Random rGenerator = new Random();
+        ArrayList<String> topicos = new ArrayList<>(Arrays.asList("3.4.1 e 3.4.2", "3.4.3", "3.4.4", "3.4.5"));
+        for (Alunos aluno : Alunos.values()) {
+            int t = rGenerator.nextInt(topicos.size());
+            aluno.topico = topicos.get(t);
+            topicos.remove(t);
+        }
+        System.out.println(Arrays.toString(Alunos.values()));
+    }
+
+}
